5ebf6d3dedfb8bfdc545b90913944881f869c9e9
Mike Houben <mike@houbsi.net>
2024-03-01 03:15:25 +0100

    Adding "Kein Artikel vorhanden."

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,14 +1,20 @@
 <x-app-layout>
     <div class="max-w-5xl mx-auto py-6 px-2">
         <ul class="divide-y">
-            @foreach($posts as $post)
-                <li class="py-4 px-2">
-                    <a href="{{ route('posts.show', $post) }}" class="text-xl font-semibold block">{{ $post->title }}</a>
-                    <span class="text-sm text-gray-600">
-                        {{ $post->created_at->diffForHumans() }} by {{ $post->user->name }}
-                    </span>
-                </li>
-            @endforeach
+                @empty($posts)
+                    @foreach($posts as $post)
+                        <li class="py-4 px-2">
+                            <a href="{{ route('posts.show', $post) }}" class="text-xl font-semibold block">{{ $post->title }}</a>
+                            <span class="text-sm text-gray-600">
+                                {{ $post->created_at->diffForHumans() }} by {{ $post->user->name }}
+                            </span>
+                        </li>
+                    @endforeach
+                    @else($posts)
+                    <li class="py-4 px-2">
+                        <p> Kein Artikel vorhanden.</p>
+                    </li>
+                @endempty
         </ul>
 
         <div>
