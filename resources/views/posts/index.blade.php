c352291874cc1dc4d1d23d9a2c5436bf5fff991d
Mike Houben <mike@houbsi.net>
2024-03-01 02:44:23 +0100

    Fix typo in posts.index View

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,15 +1,12 @@
 <x-app-layout>
-    <div class="max-2-5xl mx-auto py-6 px-2">
+    <div class="max-w-5xl mx-auto py-6 px-2">
         <ul class="divide-y">
             @foreach($posts as $post)
                 <li class="py-4 px-2">
-
                     <a href="{{ route('posts.index', $post) }}" class="text-xl font-semibold block">{{ $post->title }}</a>
-
                     <span class="text-sm text-gray-600">
                         {{ $post->created_at->diffForHumans() }} by {{ $post->user->name }}
                     </span>
-
                 </li>
             @endforeach
         </ul>
