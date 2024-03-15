7a3639ca0b0ef85945b670f86a34bcea9aa1dcd9
Mike Houben <mike@houbsi.net>
2024-03-15 23:21:41 +0100

    Fixing Dark Mode Readability

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,8 +8,8 @@
                 @else
                     @foreach($posts as $post)
                         <li class="py-4 px-2">
-                            <a href="{{ route('posts.show', $post) }}" class="text-xl font-semibold block">{{ $post->title }}</a>
-                            <span class="text-sm text-gray-600">
+                            <a href="{{ route('posts.show', $post) }}" class="text-xl font-semibold block dark:text-gray-400">{{ $post->title }}</a>
+                            <span class="text-sm text-gray-500">
                                 {{ $post->created_at->diffForHumans() }} by {{ $post->user->name }}
                             </span>
                         </li>
